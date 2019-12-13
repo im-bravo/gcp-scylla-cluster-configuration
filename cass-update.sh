7137ac516787ae21d7a1433c1b707ddf2f4e79cf
maliadmin <brown888bear@gmail.com>
2019-12-13 18:58:44 +0900

    cass

--- cass-update.sh
+++ cass-update.sh
@@ -1,9 +1,9 @@
 #
 sudo systemctl stop cassandra
 #
-sudo sh -c "cat cassandra.yaml > /etc/cassandra/scylla.yaml"
+sudo sh -c "cat cassandra.yaml > /etc/cassandra/cassandra.yaml"
 #
-sudo sh -c "cat cassandra-rackdc.properties > /etc/scylla/cassandra-rackdc.properties"
+sudo sh -c "cat cassandra-rackdc.properties > /etc/cassandra/cassandra-rackdc.properties"
 # 
 sudo rm -rf /var/lib/cassandra/*
 #
