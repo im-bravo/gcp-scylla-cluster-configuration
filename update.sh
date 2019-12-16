2aee62f4c4107a96afadd0ccb0b43a421a2ee123
maliadmin <brown888bear@gmail.com>
2019-12-16 17:45:51 +0900

    d

--- update.sh
+++ update.sh
@@ -5,7 +5,8 @@ sudo sh -c "cat scylla.yaml > /etc/scylla/scylla.yaml"
 #
 sudo sh -c "cat cassandra-rackdc.properties > /etc/scylla/cassandra-rackdc.properties"
 # 
-sudo rm -rf /var/lib/scylla/*
+sudo rm -rf /var/lib/scylla/commitlog/*
+sudo rm -rf /var/lib/scylla/data/*
 #
 sudo systemctl start scylla-server
 #
