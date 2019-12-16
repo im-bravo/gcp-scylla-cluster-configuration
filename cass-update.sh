18ad1e5d4af7f86a998489caa7550a70fd160828
maliadmin <brown888bear@gmail.com>
2019-12-16 18:08:16 +0900

    d

--- cass-update.sh
+++ cass-update.sh
@@ -5,7 +5,10 @@ sudo sh -c "cat cassandra.yaml > /etc/cassandra/cassandra.yaml"
 #
 sudo sh -c "cat cassandra-rackdc.properties > /etc/cassandra/cassandra-rackdc.properties"
 # 
-sudo rm -rf /var/lib/cassandra/*
+sudo rm -rf /var/lib/cassandra/data/*
+sudo rm -rf /var/lib/cassandra/hints/*
+sudo rm -rf /var/lib/cassandra/commitlog/*
+
 #
 sudo systemctl restart cassandra
 #
